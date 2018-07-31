44df1aadba55dd1a2d649e9594999409a664425e
LT <lalit.tanwar07@gmail.com>
2018-07-31 18:14:34 +0530

    main class added

--- server-ui/src/main/java/com/amx/jax/WebApplication.java
+++ server-ui/src/main/java/com/amx/jax/WebApplication.java
@@ -1,28 +1,20 @@
 package com.amx.jax;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@ServletComponentScan
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-@ComponentScan("com.amx.jax")
-@EnableAsync
+//@ServletComponentScan
+@SpringBootApplication
+@ComponentScan("com")
+@EnableAsync(proxyTargetClass = true)
 @EnableCaching
-public class WebApplication extends SpringBootServletInitializer {
+public class WebApplication {
 
-	/*
-	 * public static void main(String[] args) {
-	 * SpringApplication.run(SampleApplication.class, args); }
-	 */
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(WebApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
 	}
+
 }
