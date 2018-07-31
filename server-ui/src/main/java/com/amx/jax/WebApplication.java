b9d700f84ea236e21d30da7278bb56429079fff0
LT <lalit.tanwar07@gmail.com>
2018-07-31 18:03:23 +0530

    Companet scan

--- server-ui/src/main/java/com/amx/jax/WebApplication.java
+++ server-ui/src/main/java/com/amx/jax/WebApplication.java
@@ -1,31 +1,28 @@
-
-
-
-
-
 package com.amx.jax;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@ServletComponentScan
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan("com.amx.jax")
-public class WebApplication extends SpringBootServletInitializer
-{
+@EnableAsync
+@EnableCaching
+public class WebApplication extends SpringBootServletInitializer {
 
 	/*
-	 * public static void main(String[] args)
-	 * {
-	 * SpringApplication.run(SampleApplication.class, args);
-	 * }
+	 * public static void main(String[] args) {
+	 * SpringApplication.run(SampleApplication.class, args); }
 	 */
 
 	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-	{
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(WebApplication.class);
 	}
 }
