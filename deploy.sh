4ce1bfe553d9945a8492effaadc2a287fbfdeac0
LT <lalit.tanwar07@gmail.com>
2018-08-01 11:36:04 +0530

    deploy scrip[t added

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 mvn -T 4 clean package -pl server-ui -am -DskipTests
-scp server-ui/target/server-ui-0.0.1-SNAPSHOT.jar $SD_USER@$SD_HOST:~/jax/server-ui-ib
-ssh -o StrictHostKeyChecking=no $SD_USER@$SD_HOST 'kill -9 $(cat /var/run/server-ui-ib/server-ui-ib.pid)'
-ssh -o StrictHostKeyChecking=no $SD_USER@$SD_HOST '/etc/init.d/server-ui-ib restart &'
+SD_USER=amxdev
+SD_HOST=10.28.42.36
+scp server-ui/target/server-ui-0.0.1-SNAPSHOT.jar $SD_USER@$SD_HOST:~/amib/server-ui
+ssh -o StrictHostKeyChecking=no $SD_USER@$SD_HOST 'kill -9 $(cat /var/run/amib-server-ui/amib-server-ui.pid)'
+ssh -o StrictHostKeyChecking=no $SD_USER@$SD_HOST '/etc/init.d/amib-server-ui restart &'
