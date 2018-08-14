c984bde96f778977cad5e62eedfe13a9390e996d
Abhishek1427 <abhishek.tiwari@almullaexchange.com>
2018-08-14 18:09:29 +0530

    Data Changed

---
+++ lib-db/src/main/java/com/amx/jax/models/FailureResponseInfo.java
@@ -0,0 +1,28 @@
+package com.amx.jax.models;
+
+public class FailureResponseInfo
+{
+	private String contactUsEmail;
+	
+	private String contactUsHelpLineNumber;
+
+	public String getContactUsEmail()
+	{
+		return contactUsEmail;
+	}
+
+	public void setContactUsEmail(String contactUsEmail)
+	{
+		this.contactUsEmail = contactUsEmail;
+	}
+
+	public String getContactUsHelpLineNumber()
+	{
+		return contactUsHelpLineNumber;
+	}
+
+	public void setContactUsHelpLineNumber(String contactUsHelpLineNumber)
+	{
+		this.contactUsHelpLineNumber = contactUsHelpLineNumber;
+	}
+}
