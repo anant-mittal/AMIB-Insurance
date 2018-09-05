8c166de0d4ecc3f64010e62beb86c553a56b669d
Abhishek1427 <abhishek.tiwari@almullaexchange.com>
2018-09-05 10:22:04 +0530

    Deployment

--- server-ui/src/main/java/com/amx/jax/controllers/RequestQuoteController.java
+++ server-ui/src/main/java/com/amx/jax/controllers/RequestQuoteController.java
@@ -3,7 +3,6 @@ package com.amx.jax.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.amx.jax.models.IncompleteApplResponse;
-import com.amx.jax.services.ActivePolicyService;
 import com.amx.jax.services.RequestQuoteService;
 import com.amx.jax.api.AmxApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,16 +16,13 @@ public class RequestQuoteController
 	String TAG = "com.amx.jax.controllers :: RequestQuoteController :: ";
 
 	private static final Logger logger = LoggerFactory.getLogger(ActivePolicyController.class);
-	
+
 	@Autowired
 	private RequestQuoteService requestQuoteService;
-	
+
 	@RequestMapping(value = "/api/reqquote/get-incomplete-policy", method = RequestMethod.POST, produces = "application/json")
 	public AmxApiResponse<IncompleteApplResponse, Object> getIncompleteApplication()
 	{
 		return requestQuoteService.getIncompleteApplication();
 	}
-	
-	
-
 }
