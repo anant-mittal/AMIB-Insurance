051f121935382ea9651decfcfa0b162da6ec1052
Abhishek1427 <abhishek.tiwari@almullaexchange.com>
2018-11-02 19:25:42 +0530

    Fixwes

--- server-ui/src/main/java/com/amx/jax/controllers/PayMentController.java
+++ server-ui/src/main/java/com/amx/jax/controllers/PayMentController.java
@@ -1,28 +1,42 @@
 package com.amx.jax.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.amx.jax.api.AmxApiResponse;
+import com.amx.jax.dict.PayGServiceCode;
+import com.amx.jax.payg.PaymentResponseDto;
+
+import src.main.java.com.amx.jax.payg.PayGService;
+import src.main.java.com.amx.jax.payg.Payment;
 
 @RestController
 public class PayMentController
 {
+	
+	@Autowired
+	private PayGService payGService;
+
 	@RequestMapping(value = "/api/payment/pay", method = { RequestMethod.POST })
-	public ResponseWrapperM<RemittanceApplicationResponseModel, AuthResponseOTPprefix> createApplication(
+	public AmxApiResponse<Object, Object> createApplication(
 			@RequestHeader(value = "mOtp", required = false) String mOtpHeader,
 			@RequestParam(required = false) String mOtp,
-			@RequestBody RemittanceTransactionRequestModel transactionRequestModel, HttpServletRequest request) {
-		ResponseWrapperM<RemittanceApplicationResponseModel, AuthResponseOTPprefix> wrapper = new ResponseWrapperM<RemittanceApplicationResponseModel, AuthResponseOTPprefix>();
+			@RequestBody Object req, HttpServletRequest request) {
 
 		// Noncompliant - exception is lost
 				Payment payment = new Payment();
-				payment.setDocFinYear(respTxMdl.getDocumentFinancialYear());
-				payment.setDocNo(respTxMdl.getDocumentIdForPayment());
-				payment.setMerchantTrackId(respTxMdl.getMerchantTrackId());
-				payment.setNetPayableAmount(respTxMdl.getNetPayableAmount());
-				payment.setPgCode(respTxMdl.getPgCode());
-
+				payment.setDocFinYear(null);
+				payment.setDocNo(null);
+				payment.setMerchantTrackId(null);
+				payment.setNetPayableAmount(null);
+				payment.setPgCode(PayGServiceCode.KNET);
 				wrapper.setRedirectUrl(payGService.getPaymentUrl(payment,
 						"https://" + request.getServerName() + "/app/landing/remittance"));
 			}
@@ -33,7 +47,6 @@ public class PayMentController
 	@RequestMapping(value = "/remit/save-remittance", method = { RequestMethod.POST })
 	public AmxApiResponse<PaymentResponseDto, Object> onPaymentCallback(
 			@RequestBody PaymentResponseDto paymentResponse) {
-		
 		
 	}
 }
