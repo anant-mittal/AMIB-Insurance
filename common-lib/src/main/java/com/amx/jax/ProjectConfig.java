ca028bc10fba10f94ffb7451006005681b94acd4
LT <lalit.tanwar07@gmail.com>
2018-11-26 18:46:32 +0530

    Table Config

--- common-lib/src/main/java/com/amx/jax/ProjectConfig.java
+++ common-lib/src/main/java/com/amx/jax/ProjectConfig.java
@@ -6,8 +6,4 @@ public final class ProjectConfig {
 	// Main Porject
 	public static Project PROJECT = Project.IB;
 
-	// Broker DB Constants
-	public static final String TABLE_EVENT_NOTIFICATION = "IRB_EVENT_NOTIFICATION";
-	public static final String VIEW_EVENT_NOTIFICATION = "IRB_V_EVENT_NOTIFICATION";
-
 }
