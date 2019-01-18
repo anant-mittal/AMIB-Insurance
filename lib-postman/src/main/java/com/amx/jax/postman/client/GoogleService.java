e22d93c78c0665239ea6bbfc18f9ceb3a11d57ce
Abhishek Tiwari <abhishek.tiwari@almullaexchange.com>
2019-01-18 11:20:08 +0530

    Contact Us Captch Test

--- lib-postman/src/main/java/com/amx/jax/postman/client/GoogleService.java
+++ lib-postman/src/main/java/com/amx/jax/postman/client/GoogleService.java
@@ -30,7 +30,11 @@ public class GoogleService {
 					.as(Map.class);
 
 			logger.info("GoogleService :: verifyCaptcha :: responseKey :" + responseKey);
-
+			logger.info("GoogleService :: verifyCaptcha :: resp :" + resp);
+			logger.info("GoogleService :: verifyCaptcha :: resp.toString() :" + resp.toString());
+			logger.info("GoogleService :: verifyCaptcha :: resp.keySet() :" + resp.keySet());
+			logger.info("GoogleService :: verifyCaptcha :: resp.values() :" + resp.values());
+			
 			if (resp != null) {
 				return ArgUtil.parseAsBoolean(resp.get("success"));
 			}
