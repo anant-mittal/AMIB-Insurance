0d21cacb6eeec4ab3f256b9eec890321db94c8a2
Abhishek Tiwari <abhishek.tiwari@almullaexchange.com>
2019-01-21 09:27:15 +0530

    Contact Us Api

--- lib-postman/src/main/java/com/amx/jax/postman/client/GoogleService.java
+++ lib-postman/src/main/java/com/amx/jax/postman/client/GoogleService.java
@@ -2,18 +2,22 @@ package com.amx.jax.postman.client;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import com.amx.jax.rest.RestService;
 import com.amx.utils.ArgUtil;
 
 @Component
 public class GoogleService {
 
+	private static final Logger logger = LoggerFactory.getLogger(GoogleService.class);
+	
 	//private String googleSecret = "6LeK33AUAAAAANWoO_wM5_3FxJ0DoPjPZp_n7pVz";
-	private String googleSecret = "6LdtFEMUAAAAACbe0JJ2MDSrsLPseUTCjb659uHD";
+	private String googleSecret = "6LedCIsUAAAAANqAW4gzuhCJzl4d4krpw8BJfBNe";
 
+	
 	@Autowired
 	RestService restService;
 
@@ -23,8 +27,12 @@ public class GoogleService {
 				.field("secret", googleSecret).field("response", responseKey).field("remoteip", remoteIP).postForm()
 				.as(Map.class);
 		if (resp != null) {
+			
+			logger.info("GoogleService :: verifyCaptcha :: responseKey :" + responseKey);
 			return ArgUtil.parseAsBoolean(resp.get("success"));
 		}
+		
+		logger.info("GoogleService :: verifyCaptcha :: remoteIP :" + remoteIP);
 		return false;
 
 	}
