4e762a22071fcc898d9d23cb6e1d09c5d313fd4f
Subodh Bhoir <subodh.bhoir@almullagroup.com>
2019-08-06 18:33:45 +0530

    remove logger

--- server-ui/src/main/java/com/amx/jax/controllers/CustomizeQuoteController.java
+++ server-ui/src/main/java/com/amx/jax/controllers/CustomizeQuoteController.java
@@ -128,26 +128,26 @@ public class CustomizeQuoteController {
 			paymentDetails.setPaymentId(paymentResponse.getPaymentId());
 			paymentDetails.setApprovalNo(paymentResponse.getAuth_appNo());
 			paymentDetails.setApprovalDate(null);
-			if(paymentResponse.getResultCode().equalsIgnoreCase(Constants.NEW_KNET_NOTCAPTURED))
+			/*if(paymentResponse.getResultCode().equalsIgnoreCase(Constants.NEW_KNET_NOTCAPTURED))
 			{
 				paymentDetails.setResultCd(Constants.NEW_KNET_MODIFIED_RESPONSE);
 				logger.info(" onPaymentCallbackNew :: paymentResponse2 :" + paymentDetails.toString());
-			}
-			//paymentDetails.setResultCd(paymentResponse.getResultCode());
+			}*/
+			paymentDetails.setResultCd(paymentResponse.getResultCode());
 			paymentDetails.setTransId(paymentResponse.getTransactionId());
 			paymentDetails.setRefId(paymentResponse.getReferenceId());
-			logger.info(" onPaymentCallbackNew :: paymentResponse3 :" + paymentDetails.toString());
+			//logger.info(" onPaymentCallbackNew :: paymentResponse3 :" + paymentDetails.toString());
 			if (null != paymentResponse.getTrackId()) {
-				logger.info(" onPaymentCallbackNew :: paymentResponse4 :" + paymentDetails.toString());
+				//logger.info(" onPaymentCallbackNew :: paymentResponse4 :" + paymentDetails.toString());
 				BigDecimal paySeqNumber = new BigDecimal(paymentResponse.getTrackId().toString());
-				logger.info(" onPaymentCallbackNew :: paySeqNumber  :" + paySeqNumber);
+				//logger.info(" onPaymentCallbackNew :: paySeqNumber  :" + paySeqNumber);
 				paymentDetails.setPaySeqNum(paySeqNumber);
 				paymentDetails.setPaymentToken(paySeqNumber.toString());
 			} else {
-				logger.info(" onPaymentCallbackNew :: paymentResponse5 :" + paymentDetails.toString());
+				//logger.info(" onPaymentCallbackNew :: paymentResponse5 :" + paymentDetails.toString());
 				paymentDetails.setPaySeqNum(null);
 			}
-			logger.info(" onPaymentCallbackNew :: paymentResponse6 :" + paymentDetails.toString());
+			//logger.info(" onPaymentCallbackNew :: paymentResponse6 :" + paymentDetails.toString());
 			PaymentDetails updateStatus = payMentService.updatePaymentDetals(paymentDetails);
 			logger.info(" onPaymentCallbackNew :: updateStatus  :" + updateStatus.toString());
 		} catch (Exception e) {
